7908350dd74922ab0ebcc3900cb0cc298d6f274c
Manix <admin@manix.info>
2017-12-11 19:22:36 +0200

    add quota test and quot amounts

--- src/Components/Quota.php
+++ src/Components/Quota.php
@@ -14,26 +14,23 @@ class Quota {
   const MONTH = 'Ym';
   const YEAR = 'Y';
   const CONVERSION_TABLE = [
-      self::MINUTE  => 60,
-      self::HOUR    => 3600,
-      self::DAY     => 86400,
-      self::WEEK    => 604800,
-      self::MONTH   => 2630016,
-      self::YEAR    => 31557600,
+      self::MINUTE => 60,
+      self::HOUR => 3600,
+      self::DAY => 86400,
+      self::WEEK => 604800,
+      self::MONTH => 2630016,
+      self::YEAR => 31557600,
   ];
 
-  public static function enforce(self $quota) {
-    $current = $quota->retrieve();
-    
-    if ($current >= $quota->threshold) {
-      throw new Exception('Quota threshold reached.', 503);
-    }
-    
-    $quota->persist($current + 1);
+  public static function enforce(self $quota, $amount = 1) {
+    if ($quota->test($amount)) {
+      throw new Exception('Quota threshold exceeded.', 503);
+    } $quota->persist($quota->getTested());
   }
 
   protected $threshold;
-  protected $timeframe;
+  protected $timeframe;/**   * Last tested value */
+  protected $tested;
 
   public function __construct($type, $id, $threshold, $timeframe = self::HOUR) {
     $this->threshold = $threshold;
@@ -41,11 +38,24 @@ class Quota {
     $this->key = 'quota/' . $type . '/' . $id . '_' . gmdate($this->timeframe);
   }
 
+  /**
+   * Returns true if quota + amount exceeds threshold or false otherwise 
+   */
+  public function test($amount = 1) {
+    $this->tested = $this->retrieve() + $amount;
+    return $this->tested > $this->threshold;
+  }
+
+  public function getTested() {
+    return $this->tested;
+  }
+
   public function retrieve() {
     return cache($this->key);
   }
-  
+
   public function persist($amount) {
     return cache($this->key, $amount, self::CONVERSION_TABLE[$this->timeframe]);
   }
+
 }
