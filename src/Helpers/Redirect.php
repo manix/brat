85c2237524260763e0eee8052eb6376f641f07f9
Manix <admin@manix.info>
2022-07-23 11:25:25 +0300

    Update Redirect.php

--- src/Helpers/Redirect.php
+++ src/Helpers/Redirect.php
@@ -7,11 +7,11 @@ class Redirect {
   /**
    * Respond with a redirect to the HTTP request.
    * @param string $url The target location.
-   * @param bool $permanent Whether to send 301 or 302 HTTP response code.
+   * @param mixed $code The response code. Can be bool ($permanent) to support legacy code.
    * @param bool $exit Whether to exit the script immediately after setting the Location header.
    */
-  public function __construct($url, $permanent = false, $exit = true) {
-    http_response_code($permanent ? 301 : 302);
+  public function __construct($url, $code = false, $exit = true) {
+    http_response_code($code < 2 ? (302 - $code) : $code);
     header('Location: ' . $url);
     
     if ($exit) {
